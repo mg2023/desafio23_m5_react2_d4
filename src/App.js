3f287dd4455ff5d1aad9be4d9df880d2367b65c9
mg <amg.vergara@gmail.com>
2023-01-10 23:30:27 -0300

    Se agrega el archivo json con la info de las pizzas, y se obtienes sus datos como si fuera una API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+import { useEffect, useState } from "react";
 
 function App() {
+  const endpoint = "/pizzas.json";
+
+  const [pizzas, setPizzas] = useState([])
+  // const sharedState = { pizzas, setPizzas }
+
+  const getPizzas = async () => {
+    const response = await fetch(endpoint)
+    let data = await response.json()
+    setPizzas(data)
+    console.log(data)
+  }
+
+  useEffect(() => {
+    getPizzas()
+  }, [])
+
+
   return (
     <div className="App">
-hola
+      hola
     </div>
   );
 }
