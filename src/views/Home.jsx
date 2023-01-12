56b43e12231698f1d913339cf688af0229f1ecb3
mg <amg.vergara@gmail.com>
2023-01-12 00:13:07 -0300

    Se agrega el backgound de pizza

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,10 +1,21 @@
+import fotopizza from '../assets/img/pizza-background2.jpg'
+import "../assets/css/home.css"
+
 export default function Home() {
     return (
-      <div >
-        <h1>Home</h1>
-  
+      <div className='pizza-background'  style={{ backgroundImage: `url(${fotopizza})`,
+      // backgroundRepeat: 'repeat',
+      // backgroundsize: '70%',
+      // height: '200px',
+      // width: '400px'
+      
+      }}>
+      Hello World
+      ssd
 
-      </div>
+      sdf
+      sdf
+    </div>
     );
   }
   
