84c3377660f7e73ceed68669f6b378e6042a8782
mg <amg.vergara@gmail.com>
2023-01-12 23:13:00 -0300

    Se agrega la image con texto para el fondo del home

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,4 +1,5 @@
-import fotopizza from '../assets/img/pizza-background2.jpg'
+// import fotopizza from '../assets/img/pizza-background2.jpg'
+import fotopizza from '../assets/img/pizza.PNG'
 import "../assets/css/home.css"
 
 export default function Home() {
@@ -7,14 +8,10 @@ export default function Home() {
       // backgroundRepeat: 'repeat',
       // backgroundsize: '70%',
       // height: '200px',
-      // width: '400px'
-      
+      // width: '400px'      
       }}>
-      Hello World
-      ssd
-
-      sdf
-      sdf
+      {/* <h1 className="text-center mt-4 mb-4">Pizzería Mamma Mia!</h1>
+      <h2 className="text-center mt-4 mb-4">Tenemos las mejores pizzas que podreas encontrar!</h2> */}
     </div>
     );
   }
