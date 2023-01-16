5245643b1d0acad9a84fdba040ec7323923f2efe
mg <amg.vergara@gmail.com>
2023-01-16 01:43:17 -0300

    Se renderiza la vista pizza

--- src/App.js
+++ src/App.js
@@ -16,23 +16,10 @@ function App() {
   const [pizzas, setPizzas] = useState([])
   const sharedPizzasContexState = { pizzas, setPizzas }
 
-  // const [pizzaSeleccionada, setPizzaSeleccionada] = useState({
-  //   "desc": " ",
-  //   "id": " ",
-  //   "img": "https://firebasestorage.googleapis.com/v0/b/apis-varias-mias.appspot.com/o/pizzeria%2Fpizza-1239077_640_cl.jpg?alt=media&token=6a9a33da-5c00-49d4-9080-784dcc87ec2c",
-  //   "ingredients": ["mozzarella", "tomates", "jamón", "orégano"],
-  //   "name": "napolitana",
-  //   "price": 5950
-  // })
-  // const sharedDescrPizzaContexState = { pizzaSeleccionada, setPizzaSeleccionada }
-
-  // console.log(pizzaSeleccionada)
-
   const getPizzas = async () => {
     const response = await fetch(endpoint)
     let data = await response.json()
     setPizzas(data)
-    // console.log(data)
   }
 
   useEffect(() => {
@@ -42,8 +29,6 @@ function App() {
 
   return (
     <div className="App">
-      
-      {/* <DescrPizzaContext.Provider value={sharedDescrPizzaContexState}> */}
       <PizzasContext.Provider value={sharedPizzasContexState}>
         <BrowserRouter>
           <Navbar />
@@ -52,10 +37,8 @@ function App() {
             <Route path="/pizza/:id" element={<Pizza />} />
             <Route path="/carrito" element={<Carrito />} />
           </Routes>
-        </BrowserRouter>
-        
+        </BrowserRouter>        
       </PizzasContext.Provider>      
-      {/* </DescrPizzaContext.Provider> */}
     </div>
   );
 }
--- src/views/Pizza.jsx
+++ src/views/Pizza.jsx
@@ -1,36 +1,86 @@
 // import { useContext, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
-import { useContext , useState, useEffect } from 'react';
+import { useContext, useState, useEffect } from 'react';
+import Button from 'react-bootstrap/Button';
 
 import PizzasContext from "../contexts/PizzasContext";
 import "../assets/css/pizza.css"
 
-
-export default function Pizza() {
-  let { id } = useParams();
-  const [pizzaSeleccionada, setPizzaSeleccionada] = useState({})
+const Pizza = () => {
+  const { id } = useParams()
   const { pizzas } = useContext(PizzasContext)
-  const pizzaIndex = pizzas.findIndex((f) => f.id === id)
+  const pizza = pizzas.filter(pizza => pizza.id === id)
 
-  useEffect(() => {
-    setPizzaSeleccionada(pizzas[pizzaIndex])       
-}, [pizzas,pizzaIndex])
+  console.log(pizzas)
+  console.log(pizza)
 
-  if (pizzaSeleccionada) {
-    return (
-    <Container className="detalle-pizza">
-      <div>
-          <img className="img-pizza-seleccionada" src={pizzaSeleccionada.img} alt="imagen de referencia pizza" />
+  return (
+    <>
+      {pizza.length > 0 ?
+        <Container className="detalle-pizza">
+          <div>
+            <img className="img-pizza-seleccionada" src={pizza[0].img} alt="imagen de referencia pizza" />
+          </div>
+          <div className='info-pizza'>
+            <div> {pizza[0].name}</div>
+            <div> {pizza[0].desc}</div>
+            <div> Ingredientes</div>
+            {console.log(pizza[0])}
+            <ul>
+              {pizza[0].ingredients.map((ingredient, i) => (
+                <li key={i}>🍕 {ingredient}</li>
+              ))}
+            </ul>
+            <div>Precio: {pizza[0].price}</div>
+            <Button variant="danger">Añadir🛒</Button>
+          </div>
+        </Container>
+        :
+        <div>
         </div>
-      <div> {pizzaSeleccionada.desc}</div>
-    </Container>
-    )
+
+      }
+    </>
+  )
 }
-else {
-    return (
-        <>
-        </>
-    )
-}
-}
+
+export default Pizza
+
+// export default function Pizza() {
+//   let { id } = useParams();
+//   const [pizzaSeleccionada, setPizzaSeleccionada] = useState({})
+//   const { pizzas } = useContext(PizzasContext)
+//   // const pizzaIndex = pizzas.findIndex((f) => f.id === id)
+
+//   // useEffect(() => {
+//   //   setPizzaSeleccionada(pizzas[pizzaIndex])
+//   // }, [pizzas, pizzaIndex])
+
+//   if (pizzaSeleccionada) {
+//     return (
+//       <Container className="detalle-pizza">
+//         <div>
+//           <img className="img-pizza-seleccionada" src={pizzaSeleccionada.img} alt="imagen de referencia pizza" />
+//         </div>
+//         <div className='info-pizza'>
+//         <div> {pizzaSeleccionada.name}</div>
+//         <div> {pizzaSeleccionada.desc}</div>
+//         {console.log(pizzaSeleccionada)}
+//         {/* <ul>
+//           {pizzaSeleccionada.ingredients.map((ingredientes) => (
+//             <li>🍕 {ingredientes}</li>
+//           ))}
+//         </ul> */}
+//         <div> {pizzaSeleccionada.price}</div>
+//         </div>
+//       </Container>
+//     )
+//   }
+//   else {
+//     return (
+//       <>
+//       </>
+//     )
+//   }
+// }
